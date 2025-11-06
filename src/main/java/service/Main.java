31a896fa971d9d4d907ec6093fc993aab0a53de4
Iamtheflash-four <salvatorecorrera04@gmail.com>
2025-11-06 15:51:16 +0100

    Add files via upload

--- src/main/java/service/Main.java
+++ src/main/java/service/Main.java
@@ -5,15 +5,16 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-public class Main 
-{
-	public static void main(String[] args)
-	{
-		ResourceConfig rc = new ResourceConfig();
-		rc.register(LoginRequest.class);
-		
-		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
-			URI.create("http://localhost:8080"), rc);
-	}
+public class Main {
+    public static void main(String[] args) {
+        // Legge la porta da variabile d'ambiente, default 8080 se non definita
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+
+        ResourceConfig rc = new ResourceConfig().packages("service");
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
+            URI.create("http://0.0.0.0:" + port + "/api/"), rc);
+
+        //System.out.println("Server avviato su http://0.0.0.0:" + port + "/api/");
+    }
 }
  
