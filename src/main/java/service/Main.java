62f947774d7cae4cd0fc6bc83f5b28cebe50b8a9
Iamtheflash-four <salvatorecorrera04@gmail.com>
2025-11-07 12:31:46 +0100

    modifica server

--- src/main/java/service/Main.java
+++ src/main/java/service/Main.java
@@ -12,9 +12,9 @@ public class Main {
 
         ResourceConfig rc = new ResourceConfig().packages("service");
         HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
-            URI.create("http://0.0.0.0:" + port + "/api/"), rc);
+            URI.create("http://0.0.0.0:" + port), rc);
 
-        //System.out.println("Server avviato su http://0.0.0.0:" + port + "/api/");
+        System.out.println("Server avviato su http://localhost:" + port + "/api/");
     }
 }
  
