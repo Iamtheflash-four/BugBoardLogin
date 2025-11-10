afed1a5798ed52888a7afd71c8e982e016f051cb
Iamtheflash-four <salvatorecorrera04@gmail.com>
2025-11-10 11:58:02 +0100

    Update Main.java

--- src/main/java/service/Main.java
+++ src/main/java/service/Main.java
@@ -10,7 +10,10 @@ public class Main {
         // Legge la porta da variabile d'ambiente, default 8080 se non definita
         int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
 
-        ResourceConfig rc = new ResourceConfig().packages("service");
+        ResourceConfig rc = new ResourceConfig()
+        .packages("service", "dto")
+        .register(org.glassfish.jersey.jsonb.JsonBindingFeature.class);
+
         HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
             URI.create("http://0.0.0.0:" + port), rc);
 
@@ -19,5 +22,6 @@ public class Main {
 }
 
  
+
 
 
