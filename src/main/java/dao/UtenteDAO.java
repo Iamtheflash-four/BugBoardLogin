d4f8bb70c8af27d67e60a2e4b0f2c04d4cd2baff
Francesco Cervera <francescocervera2005@gmail.com>
2025-11-25 14:19:59 +0100

    Inserimento firma metodo changePassword

--- src/main/java/dao/UtenteDAO.java
+++ src/main/java/dao/UtenteDAO.java
@@ -1,12 +1,9 @@
 package dao;
 
-import java.sql.SQLException;
-
 import entity.Utente;
 
 public interface UtenteDAO 
 {
-	public Utente getUserByCredentials(String email, String password) throws Exception;
-	
-	//public java.sql.Connection connect();
+    Utente getUserByCredentials(String email, String password) throws Exception;
+    void changePassword(String email, String oldPassword, String newPassword) throws Exception;
 }
