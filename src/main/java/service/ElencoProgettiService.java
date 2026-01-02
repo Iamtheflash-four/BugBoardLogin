1acb070c5aab751b5fee41b2843b59ab25e1b9ed
Iamtheflash-four <salvatorecorrera04@gmail.com>
2026-01-02 10:35:01 +0100

    Add files via upload

---
+++ src/main/java/service/ElencoProgettiService.java
@@ -0,0 +1,37 @@
+package service;
+
+import java.util.ArrayList;
+
+import dao.ProgettoPostgresDAO;
+import dto.ElencoProgettiResponse;
+import entity.Progetto;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/elenchi")
+public class ElencoProgettiService 
+{
+	@GET
+    @Path("/progetti")
+    @Consumes(MediaType.APPLICATION_JSON)   
+    @Produces(MediaType.APPLICATION_JSON) 
+	public static Response elencaProgetti(@HeaderParam("token") String token)
+	{
+		try {
+			int idUtente = new TokenGenerator(System.getenv("JWT_SECRET")).validateUserTokenAndGetID(token);
+			ArrayList<Progetto> elenco = new ProgettoPostgresDAO().elencaProgettiUtente(idUtente);
+			if(elenco == null || elenco.isEmpty())
+				System.out.println("Vuoto");
+			return Response.status(Response.Status.OK)
+					.entity(new ElencoProgettiResponse(null, elenco)).build();
+		} catch (Exception e) {
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+	                .entity(new ElencoProgettiResponse(e.getMessage(), null)).build();
+		}
+	}	
+}
